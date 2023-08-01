fe291b5b29c61c57d10d12feef8f8eab5cb261a5
Paula2409 <padgiromano@gmail.com>
2023-08-01 07:44:30 -0300

    Ejercicio 16

---
+++ max_product_of_three.py
@@ -0,0 +1,75 @@
+"""
+A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000]."""
+
+def solution(A):
+
+    # Ordenamos el array
+    for k in range(len(A)):
+        minimal = k
+
+        for i in range(k + 1, len(A)):
+            if A[i] < A[minimal]:
+                minimal = i
+
+        A[k], A[minimal] = A[minimal], A[k]
+    print(A)
+
+    maximal_triple_1 = A[-3] * A[-2] * A[-1]
+    maximal_triple_2 = A[0] * A[1] * A[-3]
+    maximal_triple_3 = A[0] * A[1] * A[-2]
+    maximal_triple_4 = A[0] * A[1] * A[-1]
+
+    # En codility utilice el metodo max()
+    if maximal_triple_2 > maximal_triple_1 and maximal_triple_2 > maximal_triple_3 and maximal_triple_2 > maximal_triple_4:
+
+      print(maximal_triple_2)
+
+    elif maximal_triple_3 > maximal_triple_1 and maximal_triple_3 > maximal_triple_2 and maximal_triple_3 > maximal_triple_4:
+
+      print(maximal_triple_3)
+      
+    elif maximal_triple_4 > maximal_triple_1 and maximal_triple_4 > maximal_triple_2 and maximal_triple_4 > maximal_triple_3:
+
+      print(maximal_triple_4)
+
+    else:
+
+      print(maximal_triple_1)
+
+solution([-3,1,2,-2,5,6])
